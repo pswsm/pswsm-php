ce545e9eb26951ba6a8b3ca0f68ffb61afecf3d6
Pau Figueras <pau@pswsm.cat>
2022-10-06 19:54:30 +0200

    Finish e07

--- exercicis2/e07/index.php
+++ exercicis2/e07/index.php
@@ -1,6 +1,11 @@
 <?php
 include "lib.php";
 $monedes = \pswsm\monedes\getMonedes();
+
+if (\filter_has_var(INPUT_POST, "submit")) {
+	$canvi = \pswsm\monedes\canvi(filter_input(INPUT_POST, "mOrigen"), filter_input(INPUT_POST, "mOrigQtt"), filter_input(INPUT_POST, "mDesti"));
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,29 +16,32 @@ $monedes = \pswsm\monedes\getMonedes();
 <body>
 	<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
 		<fieldset>
-			<legend>Order your patates here</legend> 
+			<legend>Coin exchange rate</legend> 
 			<div>
-				<label for="quilos">Quilos de patates:</label><br>
-				<!-- write a selector for drinks -->
 				<?php
-				//get all drink names
 				$preus = pswsm\monedes\getMonedes();
-				//DEBUG: add a test drink not in initial array to check drink not found error (REMOVE FOR PRODUCTION!!!)
-				//echo selector with all drink names
-				patates_dom\renderSlider("quilos", $preus);
 				?>
-				<button type="submit" name="btnSubmit" value="submit">Submit</button>
+				<select id="mOrigen" name="mOrigen">
+				<?php
+				\pswsm\monedes\monedesDom($preus);
+				?>
+				</select>
+				<input type="number" name="mOrigQtt">
+				<span>&nbsp;to&nbsp;</span>
+				<select id="mDesti" name="mDesti">
+				<?php
+				\pswsm\monedes\monedesDom($preus);
+				?>
+				</select>
+				<br>
+				<button type="submit" name="submit" value="submit">Submit</button>
 			</div>
 		</fieldset>
 		<fieldset>
 			<legend>Receipt</legend> 
 			<div>
-				<label for="selectedDrink">Pes: </label>
-				<input type="text" name="quilos" disabled="disabled" value="<?php echo $selectedQuilos . " kg" ?? ""; ?>" />
-			</div>
-			<div>
-				<label for="price">Unit price: </label>
-				<input type="text" name="price" disabled="disabled" value="<?php echo number_format($price ?? 0.0, 2); ?>" />
+				<label for="price">Canvi: </label>
+				<input type="text" name="price" disabled="disabled" value="<?php isset($canvi) ? printf('%f', $canvi) : printf(''); ?>" />
 			</div>
 		</fieldset>
 	</form>
