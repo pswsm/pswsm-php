862a763ea9c21a76eeef061eed32e6d75c247ecd
pswsm <pauseawesome@protonmail.com>
2022-10-18 19:29:34 +0200

    Practica PHP

---
+++ pt11v0/register.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title>Login</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container-fluid">
+  <h2>Registration form</h2>
+  <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+    <div class="form-group">
+      <label for="username">Username:</label>
+      <input type="text" class="form-control" id="username" placeholder="Enter username" name="username">
+    </div>
+    <div class="form-group">
+      <label for="password">Password:</label>
+      <input type="password" class="form-control" id="password" placeholder="Enter password" name="password">
+    </div>
+    <div class="form-group">
+      <label for="name">Name:</label>
+      <input type="text" class="form-control" id="name" placeholder="Enter name" name="name">
+    </div>
+    <div class="form-group">
+      <label for="surname">Surname:</label>
+      <input type="text" class="form-control" id="surname" placeholder="Enter surname" name="surname">
+    </div>
+    <button type="submit" name="registersubmit" class="btn btn-default">Submit</button>
+  </form>
+</div>
+</body>
+</html>
---
+++ pt11v0/topmenu.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-default">
+<div class="container col-md-10">
+<div class="navbar-header">
+<a class="navbar-brand" href="https://www.proven.cat">ProvenSoft</a>
+</div>
+<ul class="nav navbar-nav">
+<li><a href='index.php'>Home</a></li>
+<li><a href='daymenu.php'>Day Menu</a></li>
+<li><a href='register.php'>Register</a></li>
+<li><a href='login.php'>Login</a></li>
+</ul>
+</div>
+</nav>
