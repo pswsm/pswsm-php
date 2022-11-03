40f6d3b1b2e64fced66ad93aeef5e58436e177ab
Pau Figueras <pau@pswsm.cat>
2022-11-03 18:16:29 +0100

    Do menus again because I merged remote instead of local 🤡

--- pt11v0/libs/navbarDom.php
+++ pt11v0/libs/navbarDom.php
@@ -1,8 +1,39 @@
 <?php namespace practica\dom {
-function mkNavDom(string $user, string $role, string $name, string $surname): string {
+function mkNavDom(string $user = '', string $role = '', string $name = '', string $surname = ''): string {
+	$register = mkMenu("Register", "register.php");
+	$login = mkMenu("Login", "login.php");
+	$logout = mkMenu("Logout", "logout.php");
+	$home = mkMenu("Home", "index.php");
+	$daymenu = mkMenu("Day Menu", "menus/daymenu.php");
+	$viewmenu = mkMenu("View Menus", "menus/viewmenus.php");
+	$admmenus = mkMenu("Admin Menus", "menus/adminmenus.php");
+	$admusers = mkMenu("Admin Users", "menus/adminusers.php");
 	$persinfo = "$name $surname";
 	$userinfo = "Logged in as: $user.";
 	$roleinfo = "You are: $role";
-	return "<p>$persinfo<br>$userinfo<br>$roleinfo</p>";
+	switch ($role) {
+		case 'registered':
+			$nav = "<ul class='nav navbar-nav'>$home$daymenu$viewmenu$logout</ul><p class='navbar-text navbar-right'>$persinfo<br>$userinfo</p><p class='navbar-text navbar-right'>$roleinfo</p>";
+			break;
+		
+		case 'staff':
+			$nav = "<ul class='nav navbar-nav'>$home$daymenu$viewmenu$admmenus$logout</ul><p class='navbar-text navbar-right'>$persinfo<br>$userinfo</p><p class='navbar-text navbar-right'>$roleinfo</p>";
+			break;
+		
+		case 'administrator':
+			$nav = "<ul class='nav navbar-nav'>$home$daymenu$viewmenu$admmenus$admusers$logout</ul><p class='navbar-text navbar-right'>$persinfo<br>$userinfo</p><p class='navbar-text navbar-right'>$roleinfo</p>";
+			break;
+		
+		default:
+			$home = mkMenu("Home", "index.php");
+			$daymenu = mkMenu("Day Menu", "menus/daymenu.php");
+			$nav = "<ul class='nav navbar-nav'>$home$daymenu$register$login</ul><p class='navbar-text navbar-right'>Not logged in</p>";
+			break;
+	}
+	return $nav;
+}
+
+function mkMenu(string $title, string $href): string {
+	return "<li><a href=$href>$title</a></li>";
 }
 } ?>
--- pt11v0/topmenu.php
+++ pt11v0/topmenu.php
@@ -7,22 +7,11 @@ use practica\dom as dom;
 <div class="navbar-header">
 <a class="navbar-brand" href="https://www.proven.cat">ProvenSoft</a>
 </div>
-<ul class="nav navbar-nav">
-<li><a href='index.php'>Home</a></li>
-<li><a href='daymenu.php'>Day Menu</a></li>
-<li><a href='register.php'>Register</a></li>
-<li><a href='login.php'>Login</a></li>
 <?php
-if (isset($user)) {
-	echo "<li><a href='logout.php'>Logout</a></li>";
-}
-?>
-</ul>
-<?php 
-if (isset($user, $name, $role, $surn)) {
+if (isset($user, $name, $surn, $role)) {
 	echo dom\mkNavDom($user, $role, $name, $surn);
 } else {
-	echo "Not logged in.";
+	echo dom\mkNavDom();
 }
 ?>
 </div>
