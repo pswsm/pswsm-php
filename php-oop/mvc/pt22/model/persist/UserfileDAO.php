73ed24612d3297f0e266d330aff9e848818d02db
Pau Figueras <pau@pswsm.cat>
2022-11-29 20:45:52 +0100

    Start working on Data Access Layer

---
+++ php-oop/mvc/pt22/model/persist/UserfileDAO.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * DAO for User objects
+ */
+class UserfileDAO {
+	/**
+	 * fetches all users from a data source
+	 * @return array of user objects or null
+	 */
+	public function fetchAll(): array|null {
+		// todo - method
+		return null;
+	}
+}
+?>
